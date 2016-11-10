f11d0489b7f873acc365199c2782fce899080848
Telerik Build User <justdecompilepublicfeedback@telerik.com>
2016-11-10 14:38:27 +0000

    Auto-Build: Version Update [***NO_CI***]
    git-tfs-id: [http://tfs:8082/]$/Behemoth/Trunk/Source/OpenSource;C959817

--- CommonVersionAssemblyInfo.cs
+++ CommonVersionAssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2016.3.1110.0")]
-[assembly: AssemblyFileVersion("2016.3.1110.0")]
+[assembly: AssemblyVersion("2016.3.1110.1")]
+[assembly: AssemblyFileVersion("2016.3.1110.1")]
